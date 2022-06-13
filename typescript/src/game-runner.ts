f2cb468b99f1bdb97b7a727db3a311b8cd913fa9
Dan Fletcher <dan.fletcher@humi.ca>
2022-06-13 16:59:53 -0400

    Set up some fixtures and almost wrote a test

--- typescript/src/game-runner.ts
+++ typescript/src/game-runner.ts
@@ -1,27 +1,25 @@
-import {Game} from './game';
+import rolls from "../tests/rolls";
+import answers from "../tests/answers";
+import { Game } from "./game";
 
 export class GameRunner {
-    public static main(): void {
-        const game = new Game();
-        game.add("Chet");
-        game.add("Pat");
-        game.add("Sue");
+  public static main(): void {
+    const game = new Game();
+    game.add("Chet");
+    game.add("Pat");
+    game.add("Sue");
 
-        let notAWinner;
-        do {
+    let notAWinner;
+    do {
+      game.roll(rolls.shift());
 
-            game.roll(Math.floor(Math.random() * 6) + 1);
-        
-            if (Math.floor(Math.random() * 10) == 7) {
-            notAWinner = game.wrongAnswer();
-            } else {
-            notAWinner = game.wasCorrectlyAnswered();
-            }
-        
-        } while (notAWinner);
-    }
+      if (answers.shift()) {
+        notAWinner = game.wrongAnswer();
+      } else {
+        notAWinner = game.wasCorrectlyAnswered();
+      }
+    } while (notAWinner);
+  }
 }
 
 GameRunner.main();
-
-  
--- typescript/tests/game.test.ts
+++ typescript/tests/game.test.ts
@@ -1,14 +1,26 @@
-import {expect} from 'chai';
-import {describe, it} from 'mocha';
-import {GameRunner} from '../src/game-runner';
+import { expect } from "chai";
+import { describe, it } from "mocha";
+import { GameRunner } from "../src/game-runner";
+import * as readline from "node:readline";
 
-describe('The test environment', () => {
-    it('should pass', () => {
-        expect(true).to.be.true;
+describe("The test environment", () => {
+  it("should pass", () => {
+    expect(true).to.be.true;
+  });
+
+  it("should access game", function () {
+    expect(GameRunner).to.not.be.undefined;
+  });
+
+  it("simulates the game", () => {
+    GameRunner.main();
+
+    const lineReader = require("readline").createInterface({
+      input: require("fs").createReadStream("file.in"),
     });
 
-    it("should access game", function () {
-        expect(GameRunner).to.not.be.undefined;
+    lineReader.on("line", function (line) {
+      console.log("Line from file:", line);
     });
-
+  });
 });
