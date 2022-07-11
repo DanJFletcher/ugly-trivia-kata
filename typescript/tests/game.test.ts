5809b1d6f043fb237f8ff4cd533521065e181058
Dan Fletcher <dan.fletcher@humi.ca>
2022-07-11 08:53:28 -0400

    Switch to jest and upgrade typescript

--- typescript/tests/game.test.ts
+++ typescript/tests/game.test.ts
@@ -1,11 +1,5 @@
 import { GameRunner } from "../src/game-runner";
-import { it } from "mocha";
-
-const chai = require("chai");
-const { jestSnapshotPlugin } = require("mocha-chai-jest-snapshot");
-const { expect } = require("chai");
-
-chai.use(jestSnapshotPlugin());
+import { it, expect } from "@jest/globals";
 
 it("simulates the game", () => {
   const outputs: string[] = [];
